3bf61b95e519492606266e0fd37118be77728fb0
Nicolas Richards <nicolasrichards@me.com>
2026-02-20 17:08:06 -0500

    Initial Commit

---
+++ MLBCallups/MLBCallupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLBCallupsApp.swift
+//  MLBCallups
+//
+//  Created by Nicolas Richards on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MLBCallupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
